c9e36018a2d6fbeb714fd34ce9ccad0cdee991fe
HaniAbiy <hanna.abiy@yahoo.com>
2024-11-10 18:02:43 -0500

    my first commit

---
+++ Netfilx-Clone/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Hello Netflix</h2>
+        
+     
+       
+    </>
+  )
+}
+
+export default App
